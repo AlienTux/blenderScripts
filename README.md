625ef08a3c18be92fcb7588be45657ea257ab0cf
alientux <kellner.kurt@gmail.com>
2020-08-11 08:49:31 -0600

    Removed clutter, added options to return to default blender keymaps, modified ReadMe

--- README.md
+++ README.md
@@ -1,3 +1,74 @@
-This is a (very basic) collection of scripts I find useful.
+# Simple Blender addons & scripts
 
-Will update this readme as I add more scripts (if I manage to make them work...)
+This is a (very basic) collection of scripts I have written myself and find useful.
+
+## Dvorak Keyboard Layout
+
+The first and most important addon (for me) in here is the **Dvorak Keyboard Layout mapper**.
+
+The whole addon is called: _dvorakAddon.py_
+
+### Installation
+
+Download the .py file and install via de Blender Add-On preferences. Activate the add-on.
+
+### Usage
+
+There are only 2 commands (so far):
+
+1. Change all unmodified keyboard bindings to Dvorak Layout
+2. Restore from Dvorak Layout to Blender (QWERTY) default
+
+To run this commands you have to press the F3 key and type the word **_Dvorak_**. Both options will show up. Select which one you want and press enter. There will be a small dialog poping up telling you what just happened.
+
+### How the add-on works
+
+If you look into the .py file you will see that the addon is extremely simple. There is a conversion map that has all the default keys on the left and the converted keys on the right. If you wish to modify this layout just change the letters and symbols on the right, save the file and re-install the addon.
+
+```
+    'A': 'A',
+    'B': 'X',
+    'C': 'J',
+    'D': 'E',
+    'E': 'PERIOD',
+    'F': 'U',
+    'G': 'I',
+    'H': 'D',
+    'I': 'C',
+    'J': 'H',
+    'K': 'T',
+    'L': 'N',
+    'M': 'M',
+    'N': 'B',
+    'O': 'R',
+    'P': 'L',
+    'Q': 'QUOTE',
+    'R': 'P',
+    'S': 'O',
+    'T': 'Y',
+    'U': 'G',
+    'V': 'K',
+    'W': 'COMMA',
+    'X': 'Q',
+    'Y': 'F',
+    'Z': 'SEMI_COLON',
+    'MINUS': 'LEFT_BRACKET',
+    'EQUAL': 'RIGHT_BRACKET',
+    'LEFT_BRACKET': 'SLASH',
+    'RIGHT_BRACKET': 'EQUAL',
+    'SEMI_COLON': 'S',
+    'QUOTE': 'MINUS',
+    'COMMA': 'W',
+    'PERIOD': 'V',
+    'SLASH': 'Z',
+```
+
+## BPSRender script
+
+This script was made to easily run the BPSRender addon for multithread rendering.
+
+It is still very basic and works only if you configure the paths yourself.
+
+Additional work is needed, but is provided here in case it gives people ideas =]
+
+
--- dvorakAddon.py
+++ dvorakAddon.py
@@ -1,158 +1,174 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+#
 # Managed to make this very small addon to change the keymap of all my addons 
 # from QWERTY to Dvorak.
-# Originally I just this script once and the keymaps would be saved:
+# Originally I just the script once and the keymaps would be saved. However
+# there were issues with the addon keymaps that prompted me to actually
+# finish this addon. I didn't write the originalscript. Found it here
+# https://blenderartists.org/t/dvorak-key-configration-transformer/630922
 #
-# (I didn't write the script. Found it here: https://blenderartists.org/t/dvorak-key-configration-transformer/630922 )
+# The addon doesn't have an interface. You have to run everything from the
+# F3 menu and then search for
+# "Dvorak". There are 2 commands:
+# 1) To change from the default blender keymap to Dvorak
+# 2) To revert everything back to the default
 
-#import bpy
-#
-#
-## Mapping between the Qwerty keys, and what keys they are replaced with
-## when using Dvorak. In other words. Label of the key on the keyboard
-## is on the left, and the key that is registered when pressing that key is
-## on the right.
-#
-## This map has been converted for English Dvorak using an English (US) keyboard
-#conversion_map = {
-#    'A': 'A',
-#    'B': 'X',
-#    'C': 'J',
-#    'D': 'E',
-#    'E': 'PERIOD',
-#    'F': 'U',
-#    'G': 'I',
-#    'H': 'D',
-#    'I': 'C',
-#    'J': 'H',
-#    'K': 'T',
-#    'L': 'N',
-#    'M': 'M',
-#    'N': 'B',
-#    'O': 'R',
-#    'P': 'L',
-#    'Q': 'QUOTE',
-#    'R': 'P',
-#    'S': 'O',
-#    'T': 'Y',
-#    'U': 'G',
-#    'V': 'K',
-#    'W': 'COMMA',
-#    'X': 'Q',
-#    'Y': 'F',
-#    'Z': 'SEMI_COLON',
-#    'MINUS': 'LEFT_BRACKET',
-#    'EQUAL': 'RIGHT_BRACKET',
-#    'LEFT_BRACKET': 'SLASH',
-#    'RIGHT_BRACKET': 'EQUAL',
-#    'SEMI_COLON': 'S',
-#    'QUOTE': 'MINUS',
-#    'COMMA': 'W',
-#    'PERIOD': 'V',
-#    'SLASH': 'Z',
-#}
-#
-#
-#wm = bpy.context.window_manager
-#
-#
-## Transform the user keys using the conversion_map
-#for km in wm.keyconfigs.user.keymaps:
-#    for kmi in km.keymap_items:
-#        if kmi.key_modifier in conversion_map:
-#            kmi.key_modifier = conversion_map[kmi.key_modifier]
-#        
-#        if kmi.type in conversion_map:
-#            kmi.type = conversion_map[kmi.type]
-#
-## Transform the addon keys using the conversion_map
-#for km in wm.keyconfigs.addon.keymaps:
-#    for kmi in km.keymap_items:
-#        if kmi.key_modifier in conversion_map:
-#            kmi.key_modifier = conversion_map[kmi.key_modifier]
-#        
-#        if kmi.type in conversion_map:
-#            kmi.type = conversion_map[kmi.type]
-
-# However, if you run the script once then all the user keymaps do change and
-# are seved, but the addon keymaps are just changed and not registered
-# as changed. Therefore, the script must be run every single time Blender
-# starts up.
-# 
-# The original addon was found here: https://blender.stackexchange.com/questions/135361/blender-2-8-startup-function-working-in-addon
-#
-# It was modified to suit my needs.
-
-bl_info = {
-    "name": "Dvorak Keymap",
-    "author": "AlienTux",
-    "version": (0, 0),
-    "blender": (2, 80, 0),
-    "description": "Sets addon keymap to Dvorak",
-    "category": "User"}
 
 import bpy
-from bpy.app.handlers import persistent
+ 
+bl_info = {
+    'name': 'Dvorak Layout',
+    'category': 'Interface',
+    'author': 'AlienTux',
+    'version': (0, 0, 1),
+    'blender': (2, 80, 0),
+    'location': 'Press \'F3\' then type type "Dvorak" to see options',
+    'description': 'Change all keybindings to Dvorak Layout'
+}
 
-@persistent
-def dvorakKeymap(scene):
-    wm = bpy.context.window_manager
-
-    conversion_map = {
-            'A': 'A',
-            'B': 'X',
-            'C': 'J',
-            'D': 'E',
-            'E': 'PERIOD',
-            'F': 'U',
-            'G': 'I',
-            'H': 'D',
-            'I': 'C',
-            'J': 'H',
-            'K': 'T',
-            'L': 'N',
-            'M': 'M',
-            'N': 'B',
-            'O': 'R',
-            'P': 'L',
-            'Q': 'QUOTE',
-            'R': 'P',
-            'S': 'O',
-            'T': 'Y',
-            'U': 'G',
-            'V': 'K',
-            'W': 'COMMA',
-            'X': 'Q',
-            'Y': 'F',
-            'Z': 'SEMI_COLON',
-            'MINUS': 'LEFT_BRACKET',
-            'EQUAL': 'RIGHT_BRACKET',
-            'LEFT_BRACKET': 'SLASH',
-            'RIGHT_BRACKET': 'EQUAL',
-            'SEMI_COLON': 'S',
-            'QUOTE': 'MINUS',
-            'COMMA': 'W',
-            'PERIOD': 'V',
-            'SLASH': 'Z',
-    }
-    for km in wm.keyconfigs.addon.keymaps:
-        for kmi in km.keymap_items:
-            if kmi.key_modifier in conversion_map:
-                kmi.key_modifier = conversion_map[kmi.key_modifier]
-            if kmi.type in conversion_map:
-                kmi.type = conversion_map[kmi.type]
-    bpy.app.handlers.depsgraph_update_post.remove(dvorakKeymap)
+# This is the conversion map for all keys. QWERTY on the left, Dvorak on the
+# right. If you wish to modify this structure just change the keys and
+# re-install the addon.
+conversion_map = {
+    'A': 'A',
+    'B': 'X',
+    'C': 'J',
+    'D': 'E',
+    'E': 'PERIOD',
+    'F': 'U',
+    'G': 'I',
+    'H': 'D',
+    'I': 'C',
+    'J': 'H',
+    'K': 'T',
+    'L': 'N',
+    'M': 'M',
+    'N': 'B',
+    'O': 'R',
+    'P': 'L',
+    'Q': 'QUOTE',
+    'R': 'P',
+    'S': 'O',
+    'T': 'Y',
+    'U': 'G',
+    'V': 'K',
+    'W': 'COMMA',
+    'X': 'Q',
+    'Y': 'F',
+    'Z': 'SEMI_COLON',
+    'MINUS': 'LEFT_BRACKET',
+    'EQUAL': 'RIGHT_BRACKET',
+    'LEFT_BRACKET': 'SLASH',
+    'RIGHT_BRACKET': 'EQUAL',
+    'SEMI_COLON': 'S',
+    'QUOTE': 'MINUS',
+    'COMMA': 'W',
+    'PERIOD': 'V',
+    'SLASH': 'Z',
+}
 
 
+# This message box I got from here: 
+# https://blender.stackexchange.com/questions/109711/how-to-popup-simple-message-box-from-python-console
+def showMessageBox(message = "", title = "Message Box", icon = 'INFO'):
+
+    def draw(self, context):
+        self.layout.label(text = 	message)
+
+    bpy.context.window_manager.popup_menu(draw, title = title, icon = icon)
+
+ 
+class dvorakLayout(bpy.types.Operator):
+    bl_idname = 'keymap.dvorak_layout'
+    bl_label = 'Change all unmodified keyboard bindings to Dvorak Layout'
+    bl_options = {"REGISTER", "UNDO"}
+
+    def execute(self, context):
+
+        wm = bpy.context.window_manager
+
+    # Transform the user keys using the conversion_map
+        for km in wm.keyconfigs.user.keymaps:
+            # get the active version of the keymap, dont alter the oiginal
+            # Addon keymap
+            km = km.active()
+            for kmi in km.keymap_items:
+                # check if keymap has already been changed, don't alter if
+                # already changed
+                if kmi.is_user_modified == False:
+                    if kmi.key_modifier in conversion_map:
+                        kmi.key_modifier = conversion_map[kmi.key_modifier]
+                    
+                    if kmi.type in conversion_map:
+                        kmi.type = conversion_map[kmi.type]
+                        
+    # Transform the addon keys using the conversion_map
+    # This section has been commented out and is not required. It is here
+    # for historical purposes. If you uncomment this and run the addon your
+    # addon keymaps will get messed up (basically the keymaps will be changed
+    # twice.
+#        for km in wm.keyconfigs.addon.keymaps:
+#           for kmi in km.keymap_items:
+#               if kmi.key_modifier in conversion_map:
+#                   kmi.key_modifier = conversion_map[kmi.key_modifier]
+#        
+#               if kmi.type in conversion_map:
+#                   kmi.type = conversion_map[kmi.type]
+
+
+    # Ask the user for a name to save the preset as
+    # This was in the original script. It is really not needed since the addon
+    # provides a means to return to the default blender config.
+#        bpy.ops.wm.keyconfig_preset_add('INVOKE_DEFAULT')
+                        
+        showMessageBox("Layout converted to Dvorak", "Dvorak Keyboard")
+            
+        return {"FINISHED"}
+
+class dvorakLayoutRestore(bpy.types.Operator):
+    bl_idname = 'keymap.dvorak_layout_restore'
+
+    bl_label = 'Restore from Dvorak Layout to Blender (QWERTY) default'
+    bl_options = {"REGISTER", "UNDO"}
+
+    def execute(self, context):
+
+        wm = bpy.context.window_manager
+        
+        # Transform the user keys using the conversion_map
+        for km in wm.keyconfigs.user.keymaps:
+            # get the active version of the keymap, don't alter the oiginal
+            # Addon keymap
+            km.restore_to_default()
+            
+        showMessageBox("Layout reverted to QWERTY")
+        
+        return {"FINISHED"}
+ 
 def register():
-    bpy.app.handlers.depsgraph_update_post.append(dvorakKeymap)
-
+    bpy.utils.register_class(dvorakLayout)
+    bpy.utils.register_class(dvorakLayoutRestore)
+ 
 def unregister():
-    bpy.app.handlers.depsgraph_update_post.remove(dvorakKeymap)
-
-if __name__ == "__main__":
-    register() 
-
-
-
-
-
+    bpy.utils.register_class(dvorakLayoutRestore)
+    bpy.utils.unregister_class(dvorakLayout)
+ 
+ 
+if __name__ == '__main__':
+    register()
